fdb75436317c4ef2004c84ad655500e83f890581
FabioReact <fabio.react.dev@gmail.com>
2023-07-03 15:55:27 +0200

    feat: explanation of useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import classes from './App.module.css'
 import Heroes from './pages/Heroes'
 
 function App() {
+  const [visible, setVisible] = useState(true)
   return (
     <>
       <h1 className={classes.red}>Vite + React + Typescript</h1>
-      <Heroes />
+      <button onClick={() => setVisible(b => !b)}>Toggle Heroes Page</button>
+      {visible ? <Heroes /> : null}
     </>
   )
 }
--- src/pages/Heroes.tsx
+++ src/pages/Heroes.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const arrayOfLetters: Array<string> = [];
 for (let i = 97; i <= 122; i++) {
@@ -6,24 +6,46 @@ for (let i = 97; i <= 122; i++) {
 }
 
 const Heroes = () => {
-	const [selectedLetter, setSelectedLetter] = useState<string>("");
+	const [selectedLetter, setSelectedLetter] = useState<string>("a");
 	// When i click on a letter, the li tag have to turn red
 	const callback = (letter: string) => {
 		console.log("A list item was clicked", letter);
 		setSelectedLetter(letter);
 	};
 
+	// API call forbiden here
+
+	useEffect(() => {
+		console.log("Rendering Heroes Page - Only runs on first mount");
+		return () => {
+			console.log("Clean up effect - Empty dependency array - Will run only once, on unmount");
+		};
+	}, []);
+
+	useEffect(() => {
+		// Fetch data from backend
+		// roomId - subscribe to room with id 101
+		console.log(
+			"Rendering Heroes Page - On first mount AND each time selectedLetter changes",
+		);
+		return () => {
+			// unsubscribe from room id 101
+			console.log("Clean up effect - Will run eacg time selectedLetter changes", selectedLetter);
+		};
+	}, [selectedLetter]);
+
 	return (
 		<section>
 			<h1>Heroes List</h1>
-			<p>You clicked on letter: {selectedLetter}</p>
-			<ul>
+			<ul className="flex justify-center gap-2 uppercase font-semibold text-xl">
 				{arrayOfLetters.map((letter) => (
 					<li
 						onClick={() => callback(letter)}
-						style={{
-							color: selectedLetter === letter ? "red" : "black",
-						}}
+						className={
+							selectedLetter === letter
+								? "text-red-600 cursor-pointer"
+								: "cursor-pointer"
+						}
 					>
 						{letter}
 					</li>
