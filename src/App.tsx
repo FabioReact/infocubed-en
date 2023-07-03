35d8bc5fc87e6040ebc43a04aa7e22c383baec04
FabioReact <fabio.react.dev@gmail.com>
2023-07-03 15:09:33 +0200

    chore: add tailwindcss to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import classes from './App.module.css'
-import ChildComponent from './ChildComponent'
-// import { Counter as MyCounter } from './pages/Counter'
-import Counter from './pages/Counter'
+import Heroes from './pages/Heroes'
 
 function App() {
   return (
     <>
       <h1 className={classes.red}>Vite + React + Typescript</h1>
-      <h2 style={{
-        color: 'red',
-        textAlign: 'center',
-      }}>With Fabio</h2>
-      <ChildComponent />
-      <Counter />
+      <Heroes />
     </>
   )
 }
