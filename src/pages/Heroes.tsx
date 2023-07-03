f51b1277ff8782321719d65d326e5eb9522fd884
FabioReact <fabio.react.dev@gmail.com>
2023-07-03 16:59:34 +0200

    feat: add first fetching for heroes page

--- src/pages/Heroes.tsx
+++ src/pages/Heroes.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Hero } from '../types/hero'
 
 const arrayOfLetters: Array<string> = [];
 for (let i = 97; i <= 122; i++) {
@@ -7,6 +8,7 @@ for (let i = 97; i <= 122; i++) {
 
 const Heroes = () => {
 	const [selectedLetter, setSelectedLetter] = useState<string>("a");
+	const [heroes, setHeroes] = useState<Hero[]>([]);
 	// When i click on a letter, the li tag have to turn red
 	const callback = (letter: string) => {
 		console.log("A list item was clicked", letter);
@@ -17,9 +19,20 @@ const Heroes = () => {
 
 	useEffect(() => {
 		// API call
+		fetch(`http://localhost:4000/heroes?name_like=^a`, {
+			method: "GET", // POST, PATCH, PUT, DELETE
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				setHeroes(data)
+			});
+
 		console.log("Rendering Heroes Page - Only runs on first mount");
 		return () => {
-			console.log("Clean up effect - Empty dependency array - Will run only once, on unmount");
+			console.log(
+				"Clean up effect - Empty dependency array - Will run only once, on unmount",
+			);
 		};
 	}, []);
 
@@ -31,7 +44,10 @@ const Heroes = () => {
 		);
 		return () => {
 			// unsubscribe from room id 101
-			console.log("Clean up effect - Will run eacg time selectedLetter changes", selectedLetter);
+			console.log(
+				"Clean up effect - Will run eacg time selectedLetter changes",
+				selectedLetter,
+			);
 		};
 	}, [selectedLetter]);
 
@@ -52,6 +68,9 @@ const Heroes = () => {
 					</li>
 				))}
 			</ul>
+			<div>
+				{heroes.map(hero => <p key={hero.id}>{hero.name}</p> )}
+			</div>
 		</section>
 	);
 };
