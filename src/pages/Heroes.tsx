ee126e8eab3a015f64f590ef02c43f1ef5d660f3
FabioReact <fabio.react.dev@gmail.com>
2023-07-04 10:17:21 +0200

    feat: HeroesPage - add refetch on click letter

--- src/pages/Heroes.tsx
+++ src/pages/Heroes.tsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
-import { Hero } from '../types/hero'
+import { Hero } from "../types/hero";
+import HeroCard from "../components/HeroCard";
 
 const arrayOfLetters: Array<string> = [];
 for (let i = 97; i <= 122; i++) {
 	arrayOfLetters.push(String.fromCharCode(i));
 }
+
+const getHeroesByLetter = (letter: string) => {
+	return fetch(`http://localhost:4000/heroes?name_like=^${letter}`, {
+		method: "GET", // POST, PATCH, PUT, DELETE
+	}).then((response) => response.json());
+};
 
 const Heroes = () => {
 	const [selectedLetter, setSelectedLetter] = useState<string>("a");
@@ -13,22 +20,19 @@ const Heroes = () => {
 	const callback = (letter: string) => {
 		console.log("A list item was clicked", letter);
 		setSelectedLetter(letter);
+		getHeroesByLetter(letter).then((data) => {
+			setHeroes(data);
+		});
 	};
 
 	// API call forbiden here
 
 	useEffect(() => {
-		// API call
-		fetch(`http://localhost:4000/heroes?name_like=^a`, {
-			method: "GET", // POST, PATCH, PUT, DELETE
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data);
-				setHeroes(data)
-			});
-
 		console.log("Rendering Heroes Page - Only runs on first mount");
+		getHeroesByLetter('a').then((data) => {
+			console.log(data);
+			setHeroes(data);
+		});
 		return () => {
 			console.log(
 				"Clean up effect - Empty dependency array - Will run only once, on unmount",
@@ -68,8 +72,10 @@ const Heroes = () => {
 					</li>
 				))}
 			</ul>
-			<div>
-				{heroes.map(hero => <p key={hero.id}>{hero.name}</p> )}
+			<div className="flex flex-wrap gap-4 justify-center">
+				{heroes.map((hero) => (
+					<HeroCard key={hero.id} hero={hero} />
+				))}
 			</div>
 		</section>
 	);
