0eb2fa20f25e17b058b36e4e0ff1a15b3c9fb788
FabioReact <fabio.react.dev@gmail.com>
2023-07-04 10:57:15 +0200

    feat: add Home page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,11 +5,12 @@ import {
 } from "react-router-dom";
 import Heroes from "./pages/Heroes";
 import Layout from "./hoc/Layout";
+import Home from './pages/Home'
 
 export const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<Layout />}>
-			<Route index element={<p>Home Page</p>} />
+			<Route index element={<Home />} />
 			<Route path="heroes" element={<Heroes />} />
 			<Route path="*" element={<p>404 - to implement</p>} />
 		</Route>,
