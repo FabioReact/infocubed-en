3ad286c3d65ec56512364bd74e4d089747f8295c
FabioReact <fabio.react.dev@gmail.com>
2023-07-04 12:01:54 +0200

    feat: create custom hook useGetHeroesByLetter

---
+++ src/hooks/useGetHeroesByLetter.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { Hero } from "../types/hero";
+
+const getHeroesByLetter = (
+	letter: string,
+	{
+		signal,
+	}: {
+		signal?: any;
+	} = {},
+) => {
+	// const controller = new AbortController();
+	// const signal = controller.signal;
+	// controller.abort();
+	return fetch(`http://localhost:4000/heroes?name_like=^${letter}`, {
+		method: "GET", // POST, PATCH, PUT, DELETE
+		signal: signal,
+	}).then((response) => response.json());
+};
+
+const useGetHeroesByLetter = () => {
+	const [heroes, setHeroes] = useState<Hero[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [isError, setIsError] = useState(false);
+
+	const refetch = (letter: string) => {
+		// First I set loading to true, I clean the potential error, then I make the API call with the letter the user clicked
+		// Once I have data (or an error), I set loading to false to show the result
+		setIsError(false);
+		setIsLoading(true);
+		getHeroesByLetter(letter)
+			.then((data) => {
+				setHeroes(data);
+			})
+			.catch((err) => {
+				console.log(err);
+				setIsError(true);
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
+	};
+
+	useEffect(() => {
+		// console.log("Rendering Heroes Page - Only runs on first mount");
+		const constroller = new AbortController();
+		const signal = constroller.signal;
+		// I only set loading to false if my API call was successfull
+		getHeroesByLetter("a", { signal: signal }).then((data) => {
+			setHeroes(data);
+			setIsLoading(false);
+		});
+		return () => {
+			constroller.abort();
+			// console.log(
+			// 	"Clean up effect - Empty dependency array - Will run only once, on unmount",
+			// );
+		};
+	}, []);
+
+	return {
+		isLoading: isLoading,
+		isError: isError,
+		heroes: heroes,
+		refetch: refetch,
+	};
+};
+
+export { useGetHeroesByLetter };
--- src/pages/Heroes.tsx
+++ src/pages/Heroes.tsx
@@ -1,73 +1,21 @@
 import { useEffect, useState } from "react";
-import { Hero } from "../types/hero";
 import HeroCard from "../components/HeroCard";
+import { useGetHeroesByLetter } from '../hooks/useGetHeroesByLetter'
 
 const arrayOfLetters: Array<string> = [];
 for (let i = 97; i <= 122; i++) {
 	arrayOfLetters.push(String.fromCharCode(i));
 }
 
-const getHeroesByLetter = (
-	letter: string,
-	{
-		signal,
-	}: {
-		signal?: any;
-	} = {},
-) => {
-	// const controller = new AbortController();
-	// const signal = controller.signal;
-	// controller.abort();
-	return fetch(`http://localhost:4000/heroes?name_like=^${letter}`, {
-		method: "GET", // POST, PATCH, PUT, DELETE
-		signal: signal,
-	}).then((response) => response.json());
-};
-
 const Heroes = () => {
 	const [selectedLetter, setSelectedLetter] = useState<string>("a");
-	const [heroes, setHeroes] = useState<Hero[]>([]);
-	const [isLoading, setIsLoading] = useState(true);
-	const [isError, setIsError] = useState(false);
-
+	const { isLoading, isError, heroes, refetch } = useGetHeroesByLetter();
 	const callback = (letter: string) => {
 		setSelectedLetter(letter);
-		// First I set loading to true, I clean the potential error, then I make the API call with the letter the user clicked
-		// Once I have data (or an error), I set loading to false to show the result
-		setIsError(false);
-		setIsLoading(true);
-		getHeroesByLetter(letter)
-			.then((data) => {
-				setHeroes(data);
-			})
-			.catch((err) => {
-				console.log(err);
-				setIsError(true);
-			})
-			.finally(() => {
-				setIsLoading(false);
-			});
+		refetch(letter);
 	};
 
 	// API call forbiden here
-
-	useEffect(() => {
-		// console.log("Rendering Heroes Page - Only runs on first mount");
-		const constroller = new AbortController();
-		const signal = constroller.signal;
-		// I only set loading to false if my API call was successfull
-		getHeroesByLetter("a", { signal: signal })
-			.then((data) => {
-				setHeroes(data);
-				setIsLoading(false);
-			})
-		return () => {
-			constroller.abort();
-			// console.log(
-			// 	"Clean up effect - Empty dependency array - Will run only once, on unmount",
-			// );
-		};
-	}, []);
 
 	useEffect(() => {
 		// Fetch data from backend
@@ -107,9 +55,9 @@ const Heroes = () => {
 				{isError && (
 					<p className="text-red-500">We have an error while fetching heroes</p>
 				)}
-				{!isLoading && !isError && (
-					heroes.map((hero) => <HeroCard key={hero.id} hero={hero} />)
-				)}
+				{!isLoading &&
+					!isError &&
+					heroes.map((hero) => <HeroCard key={hero.id} hero={hero} />)}
 			</div>
 		</section>
 	);
