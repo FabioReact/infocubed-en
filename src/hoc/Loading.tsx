cb654a5ffa5fc7cc13815d60ba2a6cd0bbf01c37
FabioReact <fabio.react.dev@gmail.com>
2023-07-05 09:34:32 +0200

    feat: add blank Loading hoc

---
+++ src/hoc/Loading.tsx
@@ -0,0 +1,11 @@
+type LoadingProps = {
+	isLoading: boolean
+}
+
+const Loading = ({ isLoading }: LoadingProps) => {
+	// If i'm loading, i only want to display a spinner
+	// if not, i return the child component
+	return (null)
+}
+
+export default Loading
