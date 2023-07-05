aba7403da4b87be5b230aacf9b26afbab6bccb86
FabioReact <fabio.react.dev@gmail.com>
2023-07-05 10:45:37 +0200

    chore: create assignment to Register

---
+++ src/pages/Register.tsx
@@ -0,0 +1,6 @@
+// In that component, i want a form with an input type email, and two input type password
+// when user submit the form (when he clicks on the button) we will run some validation
+// The two password should match
+// password should be at least 10 characters, maximum of a 100, should have a uppercase, a lowercase, a number and a special character
+// email should have the correct format
+// If validation succeed, log 'Success into the console'
