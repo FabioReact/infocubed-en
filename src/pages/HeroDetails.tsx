cc82671aba34ec413580c3c33500f3e5417f8fce
FabioReact <fabio.react.dev@gmail.com>
2023-07-06 12:21:24 +0200

    feat: using custom request

--- src/pages/HeroDetails.tsx
+++ src/pages/HeroDetails.tsx
@@ -1,11 +1,15 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import Fetcher, { BASE_URL } from '../api/fetcher'
 // import HeroCard from '../components/HeroCard'
-import { Hero } from '../types/hero'
-import { Suspense, lazy } from 'react'
+import { Suspense, lazy } from "react";
+import { getHeroById } from '../api/heroes'
 
-const HeroCard = lazy(() => import('../components/HeroCard'))
+const HeroCard = lazy(
+	() =>
+		new Promise(resolve => {
+			setTimeout(() => resolve(import("../components/HeroCard") as any), 1500)
+		}),
+);
 
 const HeroDetails = () => {
 	const { id } = useParams();
@@ -13,19 +17,27 @@ const HeroDetails = () => {
 	// getHeroById/5 -> fetch -> (cached)
 	// getHeroById/5 -> cached
 	// getHeroById/3 -> cached
-	const { data: hero, isLoading, isError, isSuccess, error } = useQuery({
+	const {
+		data: hero,
+		isLoading,
+		isError,
+		isSuccess,
+		error,
+	} = useQuery({
 		queryKey: ["getHeroById", id],
-		queryFn: () => Fetcher.get<Hero>(`${BASE_URL}/heroes/${id}`),
+		queryFn: () => getHeroById(id as string),
 		cacheTime: 20,
 		// enabled: false
 	});
 
 	return (
 		<section>
-			<h1>Hero Details - {id}</h1>
-			{isLoading && <p>Loading...</p>}
-			{isError && <p className='text-red-500'>Error: {(error as any)?.message}</p>}
-			<Suspense fallback={ <p>Loading Hero...</p> }>
+			<h1>Hero Details</h1>
+			{isLoading && <p>Loading Data...</p>}
+			{isError && (
+				<p className="text-red-500">Error: {(error as any)?.message}</p>
+			)}
+			<Suspense fallback={<p>Loading Hero...</p>}>
 				{isSuccess && <HeroCard hero={hero} />}
 			</Suspense>
 		</section>
